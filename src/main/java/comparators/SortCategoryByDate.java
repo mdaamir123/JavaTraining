75aefda8fa5cb0973bde4e5f6411422770cf53ec
Md.Aamir <man@narola.email>
2023-04-11 17:04:57 +0530

    Refactored the code.

--- src/main/java/comparators/SortCategoryByDate.java
+++ src/main/java/comparators/SortCategoryByDate.java
@@ -6,6 +6,9 @@ import java.util.Comparator;
 public class SortCategoryByDate implements Comparator<Category> {
     @Override
     public int compare(Category o1, Category o2) {
-        return o1.getCreatedOn().compareTo(o2.getCreatedOn());
+        if(o1.getCreatedOn().compareTo(o2.getCreatedOn()) > 0) {
+            return -1;
+        }
+        return 1;
     }
 }
